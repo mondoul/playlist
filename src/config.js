bc370a0d069e35cabd7435051df97a036d810ef7
arnaud mondoulet <arnaud.mondoulet@valtech.com>
2016-12-20 17:19:25 +0100

    Updated code to make it work on Heroku

--- src/config.js
+++ src/config.js
@@ -1,18 +1,11 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-exports.getConfig = getConfig;
-exports.getEnvironment = getEnvironment;
-var config = {
+const config = {
     local: {
         httpPort: 3001,
         playlistCacheKey: 'vimeo-playlist-data',
         vimeo: {
             clientId: 'd446795242b03ae86ec5f935431d69e24e0d851b',
             clientSecret: 'whM2szzzGKxTh4m0WvqCcqGnT52r6e+hz3h02xEaaHc6eUYPS1Gtr7jKtxQKfrpOonYfAYMQVedGnvdULfdrvQgNaEKhCjuPqTWvdfM0koXJi0hYKSSnreS+o/wvaSob',
-            accessToken: 'cd8d36f486be1b3d792ee717fe3ac33f' //'7acca8175826793fee6d57ded5cfc051'
+            accessToken: 'cd8d36f486be1b3d792ee717fe3ac33f'//'7acca8175826793fee6d57ded5cfc051'
         }
     },
     production: {
@@ -21,15 +14,15 @@ var config = {
         vimeo: {
             clientId: 'd446795242b03ae86ec5f935431d69e24e0d851b',
             clientSecret: 'whM2szzzGKxTh4m0WvqCcqGnT52r6e+hz3h02xEaaHc6eUYPS1Gtr7jKtxQKfrpOonYfAYMQVedGnvdULfdrvQgNaEKhCjuPqTWvdfM0koXJi0hYKSSnreS+o/wvaSob',
-            accessToken: 'cd8d36f486be1b3d792ee717fe3ac33f' //'7acca8175826793fee6d57ded5cfc051'
+            accessToken: 'cd8d36f486be1b3d792ee717fe3ac33f'//'7acca8175826793fee6d57ded5cfc051'
         }
     }
 };
 
-function getConfig() {
+export function getConfig() {
     return config[getEnvironment()];
 }
 
-function getEnvironment() {
+export function getEnvironment() {
     return process.env.NODE_ENV || 'local';
 }
