73dd3cd98670a2d2705a6e4cf571cdfb3a860cd0
arnaud mondoulet <arnaud.mondoulet@valtech.com>
2016-12-20 15:42:14 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,60 @@
+import express from 'express';
+import errorHandler from 'errorhandler';
+import path from 'path';
+import cors from 'cors';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import { getPlaylists } from './services/vimeoService';
+import NodeCache from 'node-cache';
+import { getConfig } from './config';
+
+const envConfig = getConfig();
+const app = express();
+const cache = new NodeCache({ checkperiod: 0});
+
+const setPlaylistCache = () => {
+    return getPlaylists().then(playlists => {
+        cache.set(envConfig.playlistCacheKey, playlists);
+        console.log('Cache set');
+        return playlists;
+    });
+};
+
+app.use(logger('combined'));
+app.use(cors());
+app.use(errorHandler({ dumpExceptions: true, showStack: true }));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// static content delivery for documentation
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set the playlists data in cache
+app.put('/playlists', (req, res) => {
+
+    setPlaylistCache().catch(err => {
+        console.log(err);
+        res.status(500).json('Couldn\'t cache the playlists.');
+    });
+
+});
+
+// Retrieve the playlists data from cache
+app.get('/playlists', (req, res) => {
+    try {
+        let playlists = cache.get(envConfig.playlistCacheKey, true);
+        res.json(playlists);
+    } catch (error) {
+        console.log(error);
+         res.json([]);
+    }
+});
+
+// Catch all other incoming requests
+app.all('*', (req, res) => res.end());
+
+// initialize cache
+setPlaylistCache().catch(err => console.error(err));
+
+export default app;
---
+++ services/vimeoService.js
@@ -0,0 +1,104 @@
+import { Vimeo } from 'vimeo';
+import { getConfig } from '../config';
+
+const envConfig = getConfig();
+const lib = new Vimeo(envConfig.vimeo.clientId, envConfig.vimeo.clientSecret, envConfig.vimeo.accessToken);
+
+export function getPlaylists() {
+
+    return new Promise((resolve, reject) => {
+       getAlbums().then(albumResults => {
+           let albumsData = [];
+           let albums = [];
+           albumResults.forEach(a => {
+               // prepare album data
+               albums.push({
+                   name: a.name,
+                   description: a.description,
+                   id: a.albumId,
+                   items: []
+               });
+
+               // Get videos from each albums
+               albumsData.push(getAlbumVideos(a.albumId));
+           });
+
+           Promise.all(albumsData).then(results => {
+               results.forEach(albumVideos => {
+                   let albumId = albumVideos[0].albumId;
+                   let videos = albumVideos.map((video, i) => {
+                       return {
+                           order: i + 1,
+                           title: video.title,
+                           description: video.description,
+                           video: video.videoLink,
+                           thumbnail: video.thumbnail
+                       }
+                   });
+
+                   albums.find(a => a.id === albumId).items.push(...videos);
+               });
+
+               resolve(albums);
+
+           }).catch(err => {
+               reject(err);
+           });
+
+       }).catch(err => {
+           reject(err);
+       });
+   });
+
+}
+
+const getAlbums = () => {
+    return new Promise((resolve, reject) => {
+        lib.request({
+            method:'GET',
+            path:'/me/albums'
+        }, (error, body) => {
+            if (error) {
+                reject(error);
+            } else {
+                let albums = body.data.map(album => {
+                    let albumId = album.uri.substr(album.uri.lastIndexOf('/') + 1);
+                    return {
+                        name: album.name,
+                        description: album.description,
+                        albumId
+                    }
+                });
+                resolve(albums);
+            }
+        });
+    });
+};
+
+const getAlbumVideos = (albumId) => {
+    return new Promise((resolve, reject) => {
+        lib.request({
+            method: 'GET',
+            path: `/me/albums/${albumId}/videos`
+        }, (error, body) => {
+            if (error)
+               reject(error);
+            else {
+                resolve(body.data.map(video => {
+                    let videoId = video.uri.substr(video.uri.lastIndexOf('/') + 1);
+                    let thumbnail = video.pictures.sizes.find(s => s.width === 640).link;
+                    let videoLink = video.files.find(f => f.quality === 'hls').link;
+
+                    return {
+                        albumId,
+                        videoId,
+                        thumbnail,
+                        videoLink,
+                        title: video.name,
+                        description: video.description
+                    };
+                }));
+            }
+        });
+    });
+};
